66db3285177d149c6c0ca556777ef4f21c6c1519
Don <dons0web@gmail.com>
2020-03-30 13:02:45 -0700

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,20 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Set to connect to an email server.
+  config.action_mailer.smtp_setting = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  # Send email in development mode.
+  config.action_mailer.perform_deliveries = true
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
